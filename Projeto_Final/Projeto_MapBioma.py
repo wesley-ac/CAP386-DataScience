c2740da67d9dd420f1267bb9365879c465cc14af
wesley-ac <wesley.campanharo@gmail.com>
2017-08-20 11:09:48 -0300

    Update Projeto_MapBioma.py

--- Projeto_Final/Projeto_MapBioma.py
+++ Projeto_Final/Projeto_MapBioma.py
@@ -15,7 +15,7 @@ import numpy as np
 
 ''' Abrindo arquivo SHP do limite do ACRE '''
 
-acre_lm = ogr.Open(r"D:\TESTE\Acre_LM.shp")
+acre_lm = ogr.Open(r"D:\TESTE\Shp\Acre_LM.shp")
 acre_ly = acre_lm.GetLayer()
 
 # Descobrindo os limites do BBox
@@ -73,14 +73,14 @@ cmax = np.max(range(0,nx,1))
     
    
 for i in range(len(tif)):
-    nomearquivo = (r"D:\TESTE\MapBioma\{0}".format(tif[i]))
+    nomearquivo = ("D:/TESTE/MapBioma/{0}".format(tif[i]))
     raster = gdal.Open(nomearquivo)
     k=0
     for r in range(0,rmax+1,1): # Numero de colunas obtidos da nx da grade
         for c in range(0,cmax+1,1):# Numero de linhas obtidos da ny da grade
             pxr = nrow+((r+1)*tgc) 
             pxc = ncol+((c+1)*tgc)
-            cobertura = raster.ReadAsArray(xoff=pxr,yoff=pxc,xsize=tgc,ysize=tgc).astype(np.float)
+            cobertura = raster.ReadAsArray(xoff=pxc,yoff=pxr,xsize=tgc,ysize=tgc).astype(np.float)
             floresta = np.where(((cobertura == 3)| (cobertura==4)| (cobertura==5)| (cobertura==6)),1.,0 )
             vegsec = np.where(((cobertura == 7)| (cobertura==8)),1.,0 )
             agric = np.where(((cobertura == 15)| (cobertura==19)| (cobertura==20)| (cobertura==21)| (cobertura==28)),1.,0 )
@@ -104,33 +104,314 @@ for i in range(len(tif)):
     print(tif[i])
 
 np.savetxt(r"D:\Teste\np_floresta_MpBioma.csv",np_floresta,delimiter=";")
-np.savetxt(r"D:\Teste\np_vegsec_MpBioma.csv",np_floresta,delimiter=";")
-np.savetxt(r"D:\Teste\np_agric_MpBioma.csv",np_floresta,delimiter=";")
+np.savetxt(r"D:\Teste\np_vegsec_MpBioma.csv",np_vegsec,delimiter=";")
+np.savetxt(r"D:\Teste\np_agric_MpBioma.csv",np_agric,delimiter=";")
 
 
 ## Calculando as regressões para cada celula
 
-#from scipy.stats import linregress
+from scipy.stats import linregress
+from numpy import genfromtxt
 
+np_floresta = np.genfromtxt("np_floresta_MpBioma.csv",delimiter=";")
+np_vegsec = np.genfromtxt("np_vegsec_MpBioma.csv",delimiter=";")
+np_agric = np.genfromtxt("np_agric_MpBioma.csv",delimiter=";")
 
-#x = (2004,2008,2010,2012,2014)
-#y = list(np_floresta[5869])
+x = (2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016)
+y = list(np_floresta[5869])
 
-#m, b, R, p, SEm = linregress(x, y)
+m, b, R, p, SEm = linregress(x, y)
 # m -declive; b: ordenada na origem; R: coeficiente de correlação (de Pearson)
 # p: p-value do teste F em que H0: y = const, independente de x
 # SEm: erro padrão do declive
 
-#regressao = np.zeros((len(np_floresta),3))
+reg_flor = np.zeros((len(np_floresta),3))
+reg_vegsec = np.zeros((len(np_floresta),3))
+reg_agric = np.zeros((len(np_floresta),3))
+dec_flor=[]
+sig_flor=[]
+dec_vegsec=[]
+sig_vegsec=[]
+dec_agric = []
+sig_agric=[]
 
-#for i in range(len(np_floresta)):
-#    y = list(np_floresta[i,])
-#    m, b, R, p, SEm = linregress(x, y)
-#    regressao[i,0] = m
-#    regressao[i,1] = R
-#    regressao[i,2] = p
+for i in range(len(np_floresta)):
+    yf = list(np_floresta[i,])
+    yv = list(np_vegsec[i,])
+    ya = list(np_agric[i,])
+    
+    # Regressao para floresta
+    m, b, R, p, SEm = linregress(x, yf)
+    reg_flor[i,0] = m
+    if m < 0 :
+        dec_flor.append('N')
+    elif m == 0 : 
+        dec_flor.append('Z')
+    elif m > 0:
+        dec_flor.append('P')
+    else:
+        dec_flor.append('')
+    reg_flor[i,1] = R
+    reg_flor[i,2] = p
+    if p<0.05:
+       sig_flor.append('S')
+    else:
+       sig_flor.append('N')
+       
+    #regressão para vegetação secundaria
+    m, b, R, p, SEm = linregress(x, yv)
+    reg_vegsec[i,0] = m
+    if m < 0 :
+        dec_vegsec.append('N')
+    elif m == 0 : 
+        dec_vegsec.append('Z')
+    elif m > 0:
+        dec_vegsec.append('P')
+    else:
+        dec_vegsec.append('')
+    reg_vegsec[i,1] = R
+    reg_vegsec[i,2] = p
+    if p<0.05:
+       sig_vegsec.append('S')
+    else:
+       sig_vegsec.append('N')
+    
+     #regressão para agricultura
+    m, b, R, p, SEm = linregress(x, ya)
+    reg_agric[i,0] = m
+    if m < 0 :
+        dec_agric.append('N')
+    elif m == 0 : 
+        dec_agric.append('Z')
+    elif m > 0:
+        dec_agric.append('P')
+    else:
+        dec_agric.append('')
+    reg_agric[i,1] = R
+    reg_agric[i,2] = p
+    if p<0.05:
+       sig_agric.append('S')
+    else:
+       sig_agric.append('N')
+    
+    
+    print("Celula: k{0}".format(i))
 
-#cabeca = ("2004;2008;2010;2012;2014;decliv;r2;p-val")
-#np.savetxt("floresta_regressao.csv",np.concatenate((np_floresta,regressao),1),header=cabeca,delimiter=";")
+#cabeca = ("2000;2001;2002;2003;2004;2005;2006;2007;2008;2009;2010;2011;2012;2013;2014;2015;2016;decliv;r2;pval")
+#np.savetxt("floresta_regressao_MpBioma.csv",np.concatenate((np_floresta,regressao),1),header=cabeca,delimiter=";")
 
 
+# Criando o arquivo shape de Grade com os atributos
+import shapefile as shp
+#import math
+
+
+z = shp.Writer(shp.POLYGON)
+#w.autoBalance = 1
+z.field("GRADE",'C')
+z.field("F2000",'F',decimal=30)
+z.field("F2001",'F',decimal=30)
+z.field("F2002",'F',decimal=30)
+z.field("F2003",'F',decimal=30)
+z.field("F2004",'F',decimal=30)
+z.field("F2005",'F',decimal=30)
+z.field("F2006",'F',decimal=30)
+z.field("F2007",'F',decimal=30)
+z.field("F2008",'F',decimal=30)
+z.field("F2009",'F',decimal=30)
+z.field("F2010",'F',decimal=30)
+z.field("F2011",'F',decimal=30)
+z.field("F2012",'F',decimal=30)
+z.field("F2013",'F',decimal=30)
+z.field("F2014",'F',decimal=30)
+z.field("F2015",'F',decimal=30)
+z.field("F2016",'F',decimal=30)
+z.field("decliv",'N',decimal=30)
+z.field("sen_decliv",'C')
+z.field("r2",'N',decimal=30)
+z.field("pval",'N',decimal=30)
+z.field("signigcativo",'C')
+
+id=0
+k=0
+
+for i in range(ny):
+    for j in range(nx):
+        vertices = []
+        parts = []
+        vertices.append([min(xmin+dx*j,xmax),max(ymin-dy*i,ymax)])
+        vertices.append([min(xmin+dx*(j+1),xmax),max(ymin-dy*i,ymax)])
+        vertices.append([min(xmin+dx*(j+1),xmax),max(ymin-dy*(i+1),ymax)])
+        vertices.append([min(xmin+dx*j,xmax),max(ymin-dy*(i+1),ymax)])
+        parts.append(vertices)
+        z.poly(parts)
+        z.record(GRADE=("R{0}C{1}".format(i,j)),
+                 F2000 = np.round(np_floresta[k,0],6), 
+                 F2001 = np.round(np_floresta[k,1],6), 
+                 F2002 = np.round(np_floresta[k,2],6), 
+                 F2003 = np.round(np_floresta[k,3],6), 
+                 F2004 = np.round(np_floresta[k,4],6), 
+                 F2005 = np.round(np_floresta[k,5],6), 
+                 F2006 = np.round(np_floresta[k,6],6), 
+                 F2007 = np.round(np_floresta[k,7],6), 
+                 F2008 = np.round(np_floresta[k,8],6), 
+                 F2009 = np.round(np_floresta[k,9],6), 
+                 F2010 = np.round(np_floresta[k,10],6), 
+                 F2011 = np.round(np_floresta[k,11],6), 
+                 F2012 = np.round(np_floresta[k,12],6), 
+                 F2013 = np.round(np_floresta[k,13],6), 
+                 F2014 = np.round(np_floresta[k,14],6), 
+                 F2015 = np.round(np_floresta[k,15],6), 
+                 F2016 = np.round(np_floresta[k,16],6), 
+                 decliv = np.round(reg_flor[k,0],6), 
+                 sen_decliv = dec_flor[k], 
+                 r2 = np.round(reg_flor[k,1],6),
+                 pval = np.round(reg_flor[k,2],6),
+                 signigcativo = sig_flor[k])
+#                 )
+        id+=1
+        k+=1
+        print("Grade-Celular {0} criada - R{1}-C{2}".format(k,i,j))
+
+z.save("grade_flor_MpBioma")
+
+
+z = shp.Writer(shp.POLYGON)
+#w.autoBalance = 1
+z.field("GRADE",'C')
+z.field("F2000",'F',decimal=30)
+z.field("F2001",'F',decimal=30)
+z.field("F2002",'F',decimal=30)
+z.field("F2003",'F',decimal=30)
+z.field("F2004",'F',decimal=30)
+z.field("F2005",'F',decimal=30)
+z.field("F2006",'F',decimal=30)
+z.field("F2007",'F',decimal=30)
+z.field("F2008",'F',decimal=30)
+z.field("F2009",'F',decimal=30)
+z.field("F2010",'F',decimal=30)
+z.field("F2011",'F',decimal=30)
+z.field("F2012",'F',decimal=30)
+z.field("F2013",'F',decimal=30)
+z.field("F2014",'F',decimal=30)
+z.field("F2015",'F',decimal=30)
+z.field("F2016",'F',decimal=30)
+z.field("decliv",'N',decimal=30)
+z.field("sen_decliv",'C')
+z.field("r2",'N',decimal=30)
+z.field("pval",'N',decimal=30)
+z.field("signigcativo",'C')
+
+id=0
+k=0
+
+for i in range(ny):
+    for j in range(nx):
+        vertices = []
+        parts = []
+        vertices.append([min(xmin+dx*j,xmax),max(ymin-dy*i,ymax)])
+        vertices.append([min(xmin+dx*(j+1),xmax),max(ymin-dy*i,ymax)])
+        vertices.append([min(xmin+dx*(j+1),xmax),max(ymin-dy*(i+1),ymax)])
+        vertices.append([min(xmin+dx*j,xmax),max(ymin-dy*(i+1),ymax)])
+        parts.append(vertices)
+        z.poly(parts)
+        z.record(GRADE=("R{0}C{1}".format(i,j)),
+                 F2000 = np.round(np_vegsec[k,0],6), 
+                 F2001 = np.round(np_vegsec[k,1],6), 
+                 F2002 = np.round(np_vegsec[k,2],6), 
+                 F2003 = np.round(np_vegsec[k,3],6), 
+                 F2004 = np.round(np_vegsec[k,4],6), 
+                 F2005 = np.round(np_vegsec[k,5],6), 
+                 F2006 = np.round(np_vegsec[k,6],6), 
+                 F2007 = np.round(np_vegsec[k,7],6), 
+                 F2008 = np.round(np_vegsec[k,8],6), 
+                 F2009 = np.round(np_vegsec[k,9],6), 
+                 F2010 = np.round(np_vegsec[k,10],6), 
+                 F2011 = np.round(np_vegsec[k,11],6), 
+                 F2012 = np.round(np_vegsec[k,12],6), 
+                 F2013 = np.round(np_vegsec[k,13],6), 
+                 F2014 = np.round(np_vegsec[k,14],6), 
+                 F2015 = np.round(np_vegsec[k,15],6), 
+                 F2016 = np.round(np_vegsec[k,16],6), 
+                 decliv = np.round(reg_vegsec[k,0],6), 
+                 sen_decliv = dec_vegsec[k], 
+                 r2 = np.round(reg_vegsec[k,1],6),
+                 pval = np.round(reg_vegsec[k,2],6),
+                 signigcativo = sig_vegsec[k])
+#                 )
+        id+=1
+        k+=1
+        print("Grade-Celular {0} criada - R{1}-C{2}".format(k,i,j))
+
+z.save("grade_vegsec_MpBioma")
+
+
+
+z = shp.Writer(shp.POLYGON)
+#w.autoBalance = 1
+z.field("GRADE",'C')
+z.field("F2000",'F',decimal=30)
+z.field("F2001",'F',decimal=30)
+z.field("F2002",'F',decimal=30)
+z.field("F2003",'F',decimal=30)
+z.field("F2004",'F',decimal=30)
+z.field("F2005",'F',decimal=30)
+z.field("F2006",'F',decimal=30)
+z.field("F2007",'F',decimal=30)
+z.field("F2008",'F',decimal=30)
+z.field("F2009",'F',decimal=30)
+z.field("F2010",'F',decimal=30)
+z.field("F2011",'F',decimal=30)
+z.field("F2012",'F',decimal=30)
+z.field("F2013",'F',decimal=30)
+z.field("F2014",'F',decimal=30)
+z.field("F2015",'F',decimal=30)
+z.field("F2016",'F',decimal=30)
+z.field("decliv",'N',decimal=30)
+z.field("sen_decliv",'C')
+z.field("r2",'N',decimal=30)
+z.field("pval",'N',decimal=30)
+z.field("signigcativo",'C')
+
+id=0
+k=0
+
+for i in range(ny):
+    for j in range(nx):
+        vertices = []
+        parts = []
+        vertices.append([min(xmin+dx*j,xmax),max(ymin-dy*i,ymax)])
+        vertices.append([min(xmin+dx*(j+1),xmax),max(ymin-dy*i,ymax)])
+        vertices.append([min(xmin+dx*(j+1),xmax),max(ymin-dy*(i+1),ymax)])
+        vertices.append([min(xmin+dx*j,xmax),max(ymin-dy*(i+1),ymax)])
+        parts.append(vertices)
+        z.poly(parts)
+        z.record(GRADE=("R{0}C{1}".format(i,j)),
+                 F2000 = np.round(np_agric[k,0],6), 
+                 F2001 = np.round(np_agric[k,1],6), 
+                 F2002 = np.round(np_agric[k,2],6), 
+                 F2003 = np.round(np_agric[k,3],6), 
+                 F2004 = np.round(np_agric[k,4],6), 
+                 F2005 = np.round(np_agric[k,5],6), 
+                 F2006 = np.round(np_agric[k,6],6), 
+                 F2007 = np.round(np_agric[k,7],6), 
+                 F2008 = np.round(np_agric[k,8],6), 
+                 F2009 = np.round(np_agric[k,9],6), 
+                 F2010 = np.round(np_agric[k,10],6), 
+                 F2011 = np.round(np_agric[k,11],6), 
+                 F2012 = np.round(np_agric[k,12],6), 
+                 F2013 = np.round(np_agric[k,13],6), 
+                 F2014 = np.round(np_agric[k,14],6), 
+                 F2015 = np.round(np_agric[k,15],6), 
+                 F2016 = np.round(np_agric[k,16],6), 
+                 decliv = np.round(reg_agric[k,0],6), 
+                 sen_decliv = dec_agric[k], 
+                 r2 = np.round(reg_agric[k,1],6),
+                 pval = np.round(reg_agric[k,2],6),
+                 signigcativo = sig_agric[k])
+#                 )
+        id+=1
+        k+=1
+        print("Grade-Celular {0} criada - R{1}-C{2}".format(k,i,j))
+
+z.save("grade_agric_MpBioma")
